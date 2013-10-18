0ce8a18c418c0f6e9524778bf638c88389689e14
Kyle Kolpek <kakolpek@gmail.com>
2013-10-18 19:14:37 -0400

    Converted Cube.cpp to use instanced drawing.

--- src/Cube.cpp
+++ src/Cube.cpp
@@ -5,44 +5,30 @@
 using namespace std;
 float Cube::vertexData[] = { 0.0,  1.0,  0.0, // BACK  // TOP LEFT VERT
                              0.0,  0.0,  0.0,          // BOT LEFT VERT
-                             1.0,  0.0,  0.0,          // BOT RIGHT VERT
-                             1.0,  0.0,  0.0,          // BOT RIGHT VERT
                              1.0,  1.0,  0.0,          // TOP RIGHT VERT
-                             0.0,  1.0,  0.0,          // TOP LEFT VERT
+                             1.0,  0.0,  0.0,          // BOT RIGHT VERT
                              1.0,  1.0,  0.0, // RIGHT
                              1.0,  0.0,  0.0,
-                             1.0,  0.0, -1.0,
-                             1.0,  0.0, -1.0,
                              1.0,  1.0, -1.0,
-                             1.0,  1.0,  0.0,
+                             1.0,  0.0, -1.0,
                              1.0,  1.0, -1.0, // FRONT
                              1.0,  0.0, -1.0,
-                             0.0,  0.0, -1.0,
-                             0.0,  0.0, -1.0,
                              0.0,  1.0, -1.0,
-                             1.0,  1.0, -1.0,
+                             0.0,  0.0, -1.0,
                              0.0,  1.0, -1.0, // LEFT
                              0.0,  0.0, -1.0,
-                             0.0,  0.0,  0.0,
-                             0.0,  0.0,  0.0,
                              0.0,  1.0,  0.0,
-                             0.0,  1.0, -1.0,
+                             0.0,  0.0,  0.0,
                              0.0,  1.0,  0.0, // TOP
                              1.0,  1.0,  0.0,
-                             1.0,  1.0, -1.0,
-                             1.0,  1.0, -1.0,
                              0.0,  1.0, -1.0,
-                             0.0,  1.0,  0.0,
+                             1.0,  1.0, -1.0,
                              0.0,  0.0,  0.0, // BOTTOM
                              0.0,  0.0, -1.0,
-                             1.0,  0.0, -1.0,
-                             1.0,  0.0, -1.0,
                              1.0,  0.0,  0.0,
-                             0.0,  0.0,  0.0,
+                             1.0,  0.0, -1.0,
                                               // NORMALS
                              0.0,  0.0,  1.0, // BACK
-                             0.0,  0.0,  1.0,
-                             0.0,  0.0,  1.0,
                              0.0,  0.0,  1.0,
                              0.0,  0.0,  1.0,
                              0.0,  0.0,  1.0,
@@ -50,11 +36,7 @@ float Cube::vertexData[] = { 0.0,  1.0,  0.0, // BACK  // TOP LEFT VERT
                              1.0,  0.0,  0.0,
                              1.0,  0.0,  0.0,
                              1.0,  0.0,  0.0,
-                             1.0,  0.0,  0.0,
-                             1.0,  0.0,  0.0,
                              0.0,  0.0, -1.0, // FRONT
-                             0.0,  0.0, -1.0,
-                             0.0,  0.0, -1.0,
                              0.0,  0.0, -1.0,
                              0.0,  0.0, -1.0,
                              0.0,  0.0, -1.0,
@@ -62,11 +44,7 @@ float Cube::vertexData[] = { 0.0,  1.0,  0.0, // BACK  // TOP LEFT VERT
                             -1.0,  0.0,  0.0,
                             -1.0,  0.0,  0.0,
                             -1.0,  0.0,  0.0,
-                            -1.0,  0.0,  0.0,
-                            -1.0,  0.0,  0.0,
                              0.0,  1.0,  0.0, // TOP
-                             0.0,  1.0,  0.0,
-                             0.0,  1.0,  0.0,
                              0.0,  1.0,  0.0,
                              0.0,  1.0,  0.0,
                              0.0,  1.0,  0.0,
@@ -74,47 +52,22 @@ float Cube::vertexData[] = { 0.0,  1.0,  0.0, // BACK  // TOP LEFT VERT
                              0.0, -1.0,  0.0,
                              0.0, -1.0,  0.0,
                              0.0, -1.0,  0.0,
-                             0.0, -1.0,  0.0,
-                             0.0, -1.0,  0.0,
-                                              // COLOR
-                             0.1,  0.8,  0.0, // BACK
-                             0.1,  0.8,  0.0,
-                             0.1,  0.8,  0.0,
-                             0.1,  0.8,  0.0,
-                             0.1,  0.8,  0.0,
-                             0.1,  0.8,  0.0,
-                             0.8,  0.0,  0.0, // RIGHT
-                             0.8,  0.0,  0.0,
-                             0.8,  0.0,  0.0,
-                             0.8,  0.0,  0.0,
-                             0.8,  0.0,  0.0,
-                             0.8,  0.0,  0.0,
-                             0.8,  0.5,  0.0, // FRONT
-                             0.8,  0.5,  0.0,
-                             0.8,  0.5,  0.0,
-                             0.8,  0.5,  0.0,
-                             0.8,  0.5,  0.0,
-                             0.8,  0.5,  0.0,
-                             0.0,  0.0,  0.8, // LEFT
-                             0.0,  0.0,  0.8,
-                             0.0,  0.0,  0.8,
-                             0.0,  0.0,  0.8,
-                             0.0,  0.0,  0.8,
-                             0.0,  0.0,  0.8,
-                             0.8,  0.8,  0.0, // TOP
-                             0.8,  0.8,  0.0,
-                             0.8,  0.8,  0.0,
-                             0.8,  0.8,  0.0,
-                             0.8,  0.8,  0.0,
-                             0.8,  0.8,  0.0,
-                             0.8,  0.8,  0.8, // BOTTOM
-                             0.8,  0.8,  0.8,
-                             0.8,  0.8,  0.8,
-                             0.8,  0.8,  0.8,
-                             0.8,  0.8,  0.8,
-                             0.8,  0.8,  0.8};
+                                              // COLOR (PER INSTANCE)
+                             0.1,  0.8,  0.0, // 1
+                             0.8,  0.0,  0.0, // 2
+                             0.8,  0.5,  0.0, // 3
+                             0.0,  0.0,  0.8, // 4
+                             0.8,  0.8,  0.0, // 5
+                             0.8,  0.8,  0.8, // 6
 
-int Cube::vertexCount = 6 * 2 * 3;
+                             0.0,  0.0,  0.0, // 1
+                             2.0,  2.0,  1.0, // 2
+                            -2.0,  0.0,  0.0, // 3
+                            -1.0,  2.0, -2.0, // 4
+                             2.0, -2.0, -1.0, // 5
+                            -1.0, -1.0, -1.0};// 6
+
+int Cube::vertexCount = 4 * 6;
 
 
 
@@ -142,7 +95,7 @@ Cube::~Cube()
 // TODO: Move this to renderer.
 GLuint Cube::loadShader()
 {
-   return shaderManager->getProgram(2, "phong.vert", "phong.frag");
+   return shaderManager->getProgram(2, "phongCube.vert", "phongCube.frag");
 }
 
 void Cube::draw(GLuint program)
@@ -150,6 +103,7 @@ void Cube::draw(GLuint program)
     GLuint vertexPosLoc = glGetAttribLocation(program, "vertexPosition");
     GLuint vertexNormalLoc = glGetAttribLocation(program, "vertexNormal");
     GLuint vertexColorLoc = glGetAttribLocation(program, "vertexColor");
+    GLuint cubePosLoc = glGetAttribLocation(program, "cubePosition");
 
     if(vertexPosLoc == -1)
     {
@@ -170,6 +124,7 @@ void Cube::draw(GLuint program)
     glEnableVertexAttribArray(vertexPosLoc);
     glEnableVertexAttribArray(vertexNormalLoc);
     glEnableVertexAttribArray(vertexColorLoc);
+    glEnableVertexAttribArray(cubePosLoc);
 
     glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
     // TODO: Change this to be described by the ModelData struct
@@ -179,11 +134,17 @@ void Cube::draw(GLuint program)
         3 * sizeof(float), (void *)(vertexCount * 3 * sizeof(float)));
     glVertexAttribPointer(vertexColorLoc, 3, GL_FLOAT, GL_FALSE,
         3 * sizeof(float), (void *)(2 * vertexCount * 3 * sizeof(float)));
+    glVertexAttribPointer(cubePosLoc, 3, GL_FLOAT, GL_FALSE,
+        3 * sizeof(float), (void *)((2 * vertexCount + 6) * 3 * sizeof(float)));
+    
+    glVertexAttribDivisor(vertexColorLoc, 1);
+    glVertexAttribDivisor(cubePosLoc, 1);
 
     // TODO: Change GL_TRIANGLES to account for modelData->vertexType
-    glDrawArrays(GL_TRIANGLES, 0, vertexCount);
+    glDrawArraysInstanced(GL_TRIANGLE_STRIP, 0, vertexCount, 6);
     glDisableVertexAttribArray(vertexPosLoc);
     glDisableVertexAttribArray(vertexNormalLoc);
     glDisableVertexAttribArray(vertexColorLoc);
+    glDisableVertexAttribArray(cubePosLoc);
     glBindBuffer(GL_ARRAY_BUFFER, 0);
 }
