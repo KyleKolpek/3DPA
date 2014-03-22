6615cc3f62da76793cfe590873b7a63c8ad583bb
Kyle Kolpek <kakolpek@gmail.com>
2014-03-21 21:48:44 -0400

    Removed block comments and fprints

--- src/main.cpp
+++ src/main.cpp
@@ -60,22 +60,23 @@ int main(int argc, char *argv[])
     if (SDL_Init(SDL_INIT_VIDEO) < 0) /* Initialize SDL's Video subsystem */
         sdldie("Unable to initialize SDL"); /* Or die on error */
  
-    /* Request opengl 3.2 context.
-     * SDL doesn't have the ability to choose which profile at this time of writing,
-     * but it should default to the core profile */
+    // Request opengl 3.2 context.
+    // SDL doesn't have the ability to choose which profile at this time of 
+    // writing, but it should default to the core profile
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
-    /*SDL_GL_SetAttribute(SDL_GL_ACCELERATED_VISUAL, 1);*/
+    //SDL_GL_SetAttribute(SDL_GL_ACCELERATED_VISUAL, 1);
  
-    /* Turn on double buffering with a 24bit Z buffer.
-     * You may need to change this to 16 or 32 for your system */
+    // Turn on double buffering with a 24bit Z buffer.
+    // You may need to change this to 16 or 32 for your system
     SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
     SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 16);
  
-    /* Hide the cursor when over the screen. This seems to be bugged. */
+    // Hide the cursor when over the screen.
+    // This seems to be bugged as it gives bad values for mouse movement.
     //SDL_SetRelativeMouseMode(SDL_TRUE);
 
-    /* Create our window centered at 512x512 resolution */
+    // Create our window centered at 512x512 resolution
     mainwindow = SDL_CreateWindow(PROGRAM_NAME,
                                   SDL_WINDOWPOS_CENTERED,
                                   SDL_WINDOWPOS_CENTERED,
@@ -91,23 +92,25 @@ int main(int argc, char *argv[])
     }
     checkSDLError(__LINE__);
 
-    /* Create our opengl context and attach it to our window */
+    // Create our opengl context and attach it to our window
     maincontext = SDL_GL_CreateContext(mainwindow);
     checkSDLError(__LINE__);
 
-    /* Initialize GLEW */
+    // Initialize GLEW
     glewExperimental = GL_TRUE;
     GLenum err = glewInit();
     if (GLEW_OK != err)
     {
-        /* Problem: glewInit failed, something is seriously wrong. */
-        fprintf(stderr, "Error: %s\n", glewGetErrorString(err));
+        // Problem: glewInit failed, something is seriously wrong.
+        std::cerr << "Error: " << glewGetErrorString(err) << endl;
     }
     
     // Gen Vertex Array Object
     glGenVertexArrays(1, &vao);
     glBindVertexArray(vao);
 
+    // Print errors, but don't react to them. This is because there is currently
+    // a 500 error that is unaccounted for in SDL2.
     while(GLenum e = glGetError())
     {
         std::cerr << "GL Error: " << std::hex << e << std::endl;
@@ -123,7 +126,8 @@ int main(int argc, char *argv[])
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
     glBlendEquation(GL_FUNC_ADD);
  
-    /* This makes our buffer swap syncronized with the monitor's vertical refresh */
+    // This makes our buffer swap syncronized with the monitor's vertical
+    // refresh.
     SDL_GL_SetSwapInterval(1);
 
     glClearColor(0.0, 0.0, 0.0, 0.0);
@@ -198,13 +202,13 @@ int main(int argc, char *argv[])
     ////////////////////////////////////////////////////////////////////////////
     while(!quit)
     {
-        /* Clear our buffer with a black background */
+        // Clear our buffer with a black background
         glClear ( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        /* Poll events */
+        // Poll events
         while(SDL_PollEvent(&event))
         {
-            /* Handle events */
+            // Handle events
             switch(event.type)
             {
                 case SDL_WINDOWEVENT:
@@ -284,25 +288,24 @@ int main(int argc, char *argv[])
         rocketContext->Update();
         rocketContext->Render();
 
-        // THIS IS HOW TO GET VALUES FROM ELEMENTS
-
         while(GLenum e = glGetError())
         {
             std::cerr << "GL Error: " << std::hex << e << std::endl;
         }
-        /* Swap our back buffer to the front */
+
+        // Swap our back buffer to the front.
         SDL_GL_SwapWindow(mainwindow);
     }
  
-    /* Delete random stuff */
+    // Delete random stuff
 
-    /* Shut down rocket */
+    // Shut down rocket
     // Example of how to unload documents
     document->GetContext()->UnloadDocument(document);
     rocketContext->RemoveReference();
     Rocket::Core::Shutdown();
 
-    /* Delete our opengl context, destroy our window, and shutdown SDL */
+    // Delete our opengl context, destroy our window, and shutdown SDL.
     SDL_GL_DeleteContext(maincontext);
     SDL_DestroyWindow(mainwindow);
     SDL_Quit();
